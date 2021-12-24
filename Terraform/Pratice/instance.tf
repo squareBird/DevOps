c69f364d65f49ffc176de1d249174e1fe1e1326e
Lux <fails9503@gmail.com>
2021-12-25 03:50:36 +0900

    0.21 - KeyPair 생성 - 보안그룹 생성(WEB, DB) - 인스턴스 생성(WEB 서버) - 보안그룹 연동 + 태그 일부 수정

--- Terraform/Pratice/instance.tf
+++ Terraform/Pratice/instance.tf
@@ -9,8 +9,15 @@ resource "aws_instance" "web-01" {
   ami = "ami-0a93a08544874b3b7"
   instance_type = "t2.micro"
   key_name = aws_key_pair.keypair.key_name
+  subnet_id = aws_subnet.main_public_subnet.id
+
   vpc_security_group_ids = [
     aws_security_group.sg_web.id
   ]
+
+  tags = {
+    Name = "WEB-01"
+    Project = var.project
+  }
 
 }
--- Terraform/Pratice/security_group.tf
+++ Terraform/Pratice/security_group.tf
@@ -1,28 +1,43 @@
 resource "aws_security_group" "sg_web" {
   name = "sg_web"
   description = "HTTP / SSH"
+  vpc_id = aws_vpc.main.id
+
   ingress {
     from_port = 80
     protocol = "tcp"
     to_port = 80
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port = 22
     protocol = "tcp"
     to_port = 22
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "sg_web"
+    Project = var.project
+  }
+
 }
 
 resource "aws_security_group" "sg_db" {
   name = "sg_db"
   description = "Allow 3306 From WEB"
+  vpc_id = aws_vpc.main.id
+
   ingress {
     from_port = 3306
     protocol = "tcp"
     to_port = 3306
-    # 이렇게 하면 WEB->DB로 오는거 적용할 수 있는지 확인
-    security_groups = [aws_security_group.sg_web]
+    # Allow from another security groups
+    security_groups = [aws_security_group.sg_web.id]
+  }
+
+  tags = {
+    Name = "sg_db"
+    Project = var.project
   }
 }
