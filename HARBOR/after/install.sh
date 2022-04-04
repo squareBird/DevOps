6711d60f198da4d5b1e3ff06d660a43eb9d98ac8
Lux <fails9503@gmail.com>
2022-04-04 19:48:20 +0900

    0.5 patch

--- HARBOR/after/install.sh
+++ HARBOR/after/install.sh
@@ -1,8 +1,7 @@
 sudo apt-get update
 sudo apt-get install docker.io
 sudo apt-get install docker-compose
-sudo apt-get install certbot
-sudo apt-get install python3-certbot-nginx
+
 
 # harbor 설치파일 다운로드
 sudo wget https://github.com/goharbor/harbor/releases/download/v2.5.0-rc2/harbor-offline-installer-v2.5.0-rc2.tgz
