b94c4f3b1b1f047d481356bfb597bfcd8533b55b
Lux <fails9503@gmail.com>
2022-06-14 13:26:38 +0900

    폴더구조 테스트

---
+++ NHN/VPC/vpc.tf
@@ -0,0 +1,18 @@
+resource "openstack_compute_instance_v2" "terraform-instance-01" {
+  name      = "terraform-instance-01"
+  region    = "KR1"
+  flavor_id = "da74152c-0167-4ce9-b391-8a88a8ff2754"
+  key_pair  = "terraform-keypair"
+  network {
+    uuid = "00d5b852-cb77-4307-b6be-d81dad24eec1"
+  }
+  security_groups = ["default"]
+  block_device {
+    uuid = "6d0993b4-cd6d-4242-b59b-94258f265331"
+    source_type = "image"
+    destination_type = "volume"
+    boot_index = 0
+    volume_size = 20
+    delete_on_termination = true
+  }
+}
